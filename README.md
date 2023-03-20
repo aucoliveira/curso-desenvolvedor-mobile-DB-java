3776553f37986c41b2c30ab2d653757ba9e9d4f9
Augusto <azotaugusto@gmail.com>
2023-03-19 22:55:56 -0300

    Correção do README

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
 </p>
 
 ## Descrição:
+<p align="justify">Desafio 06</p>
 <p align="justify"> Criação de Tabelas
  
 Agora que você já elaborou a modelagem para o projeto de banco de dados da empresa Coma Bem, é hora de criar as tabelas, utilizando linguagem de definição de banco de dados SQL. 
@@ -14,6 +15,12 @@ Agora que você já elaborou a modelagem para o projeto de banco de dados da emp
 ## Modelo:
 
 ![Captura de tela de 2023-03-19 20-24-49](https://user-images.githubusercontent.com/21234981/226217308-7ad15c3e-1988-421f-9e17-d736423b2421.png)
+
+
+## Descrição:
+<p align="justify">Desafio 07</p>
+<p align="justify">ELABORAÇÃO DE SCRIPT PARA MANIPULAÇÃO DE DADOS </p>
+<p align="justify"> Após criar as tabelas do banco de dados da empresa Coma Bem, agora elabore o script para a inserção, alteração, consulta e exclusão de dados. </p>
 
 ## Tecnologias usadas:
 
--- desafio_07.sql
+++ desafio_07.sql
@@ -2,26 +2,43 @@
 use coma_bem_db;
 
 show tables;
+
 -- CREATE 
 describe Produto;
  
 insert into Produto (nome, preco) values ('carne', 23.57);
 
-select * from Produto;
-
 describe Pedidos;
 
-insert into Pedidos (
-	id_produto, quantidade, valor)
-	values (
-	2, 3,(select po.preco
-		from Pedidos as pe
-		inner join Produto as po
-		on (pe.id_produto = po.id_produto)
-		where po.id_produto =2)
-	);
+insert into Pedidos (id_produto, quantidade, valor)	values (2, 3, 0);
 	
 select * from Pedidos;
 
+-- READ
+select * from Pedidos; 
 
-delete from Pedidos where id_pedido = 4;
+select * from Produto;
+
+select po.nome,po.preco as 'Preço unitário', pe.quantidade, pe.data, pe.quantidade * po.preco as 'Valor do Pedido'
+	from Pedidos as pe
+	inner join Produto as po on (pe.id_produto = po.id_produto);
+
+
+-- UPDATE 
+update Produto set preco = 3.78 where id_produto = 1;
+
+update Pedidos set valor =(
+		select po.preco * pe.quantidade 
+		from Pedidos as pe
+		inner join Produto as po 
+			on (pe.id_produto = po.id_produto)
+		where po.id_produto =1)
+	where id_pedido = 1;
+
+-- DELETE 
+delete from Pedidos where id_pedido = 2;
+
+ -- Deletando um produto que tem relação com outra tabela Pedido
+delete from Pedidos where id_pedido = 1;
+
+delete from Produto where id_produto  =1;
